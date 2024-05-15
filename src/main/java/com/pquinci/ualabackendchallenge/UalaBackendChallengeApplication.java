6bce083ab2176c0590f208e00f8375e4fed31e73
Pablo <pablo.quinci@gmail.com>
2024-05-15 15:17:09 -0300

    Primer commit

---
+++ src/main/java/com/pquinci/ualabackendchallenge/UalaBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.pquinci.ualabackendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UalaBackendChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UalaBackendChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pquinci/ualabackendchallenge/UalaBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pquinci.ualabackendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UalaBackendChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
